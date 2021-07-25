02d9738ca0b7cc07a6176d0c5b4cfb00686f1f98
DayKongtto <work1520@naver.com>
2021-07-25 17:18:51 +0900

    - 클래스 extension으로 나눠서 가독성 높이기 - 자동레이아웃 오류 수정

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     let cellIndentifier: String = "countryCell"
@@ -21,37 +21,19 @@ class ViewController: UIViewController, UITableViewDataSource {
         self.tableView.dataSource = self
         
         let jsonDecoder: JSONDecoder = JSONDecoder()
-        guard let dataAsset: NSDataAsset = NSDataAsset(name: "countries") else {
+        if let dataAsset: NSDataAsset = NSDataAsset(name: "countries") {
+            do {
+                self.countries = try jsonDecoder.decode([Country].self, from: dataAsset.data)
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+            self.tableView.reloadData()
+        } else {
             return
         }
-        
-        do {
-            self.countries = try jsonDecoder.decode([Country].self, from: dataAsset.data)
-        } catch {
-            print(error.localizedDescription)
-        }
-        
-        self.tableView.reloadData()
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.countries.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        
-        let cell: CountryTableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIndentifier, for: indexPath) as! CountryTableViewCell
-        
-//        cell.leftLabel.text = self.dateFormatter.string(from: self.dates[indexPath.row])
-        cell.countryLabel.text = self.countries[indexPath.row].koreanName
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "국가"
-    }
     
     // MARK: - Navigation
 
@@ -74,3 +56,24 @@ class ViewController: UIViewController, UITableViewDataSource {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell: CountryTableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIndentifier, for: indexPath) as! CountryTableViewCell
+        
+//        cell.leftLabel.text = self.dateFormatter.string(from: self.dates[indexPath.row])
+        cell.countryLabel.text = self.countries[indexPath.row].koreanName
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "국가"
+    }
+}
